c5d00eaf4dbaed0bcc3b6c30c02ab14e50e1c84e
Kate Bass <katebass@MacBook-Pro-Dmitry.local>
2023-02-08 11:28:35 +0200

    Added tests

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,1 +1,48 @@
+
+describe('chainer', () => {
 const { chainer } = require('./chainer');
+    
+    it('should return result', () => {
+        const f1 = jest.fn((x) => x - 3);
+        const f2 = jest.fn((x) => x / 3);
+        const f3 = jest.fn((x) => x + 3);
+
+        const result = chainer([f1, f2, f3])(15);
+
+        expect(result)
+            .toBe(7);
+    });
+
+    it('should return result f1', () => {
+        const f1 = jest.fn((x) => x - 3);
+        const f2 = jest.fn((x) => x / 3);
+        const f3 = jest.fn((x) => x + 3);
+
+        const result = chainer([f1, f2, f3])(15);
+
+        expect(f1.mock.results[0].value)
+            .toBe(12);
+    });
+
+    it('should return result f2', () => {
+        const f1 = jest.fn((x) => x - 3);
+        const f2 = jest.fn((x) => x / 3);
+        const f3 = jest.fn((x) => x + 3);
+
+        const result = chainer([f1, f2, f3])(15);
+
+        expect(f2.mock.results[0].value)
+            .toBe(4);
+    });
+
+    it('should return result f3', () => {
+        const f1 = jest.fn((x) => x - 3);
+        const f2 = jest.fn((x) => x / 3);
+        const f3 = jest.fn((x) => x + 3);
+
+        const result = chainer([f1, f2, f3])(15);
+
+        expect(f3.mock.results[0].value)
+            .toBe(7);
+    });
+});
